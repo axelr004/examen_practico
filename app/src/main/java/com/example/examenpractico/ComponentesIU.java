1fdadcf05ce5253a01e41a0cbf0b13753b2440ae
Nicolas24137157 <nicolasvr24137@gmail.com>
2022-05-30 00:42:42 -0500

    Adición de Componentes IU Tercera Actividad

--- app/src/main/java/com/example/examenpractico/ComponentesIU.java
+++ app/src/main/java/com/example/examenpractico/ComponentesIU.java
@@ -3,12 +3,73 @@ package com.example.examenpractico;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ComponentesIU extends AppCompatActivity {
+    TextView informacion;
+    CheckBox checkBox;
+    String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_componentes_iu);
+        informacion = findViewById(R.id.informacion);
+        checkBox = findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+               buscar ();
+            }
+        });
+        String[] nombres = {"David","Alexander","Nicolas","Cristhian"};
+        Spinner Snombres = (Spinner) findViewById(R.id.spinner);
+        Snombres.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item,nombres));
+        Snombres.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                TextView textView = (TextView)selectedItemView;
+                result = textView.getText().toString();
+                buscar();
+            }
+                 ;
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+    }
+    public  void buscar(){
+        String plan = "3743";
+
+        if(result.equals("David")){
+            result = "German David Estrada Holguin - 2013122";
+        } else if(result.equals("Nicolas")){
+            result = "Nicolas Felipe Victoria Rodriguez - 1767315";
+        } else if(result.equals("Cristhian")){
+            result = "Cristhian Camilo Lozano - 2067818";
+        } else if(result.equals("Alexander")){
+            result = "Alexander Ramirez Bohorquez - 1556236";
+            plan = "2711";
+        }
+        String adicional = "";
+
+        if(checkBox.isChecked()){
+
+            adicional = "\nPlan: "+plan+"\n Universidad del Valle";
+
+        }
+        informacion.setText(result + adicional);
+
     }
 }
